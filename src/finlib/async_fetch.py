9d5ec9eeb5bf37d6047f92a2fce6aae744e6a377
simone <284838112+bellinquente-a11y@users.noreply.github.com>
2026-07-02 10:42:37 +1000

    feat: enhance Binance data fetching with improved data validation, support for multiple rows, and conversion to DataFrame

--- src/finlib/async_fetch.py
+++ src/finlib/async_fetch.py
@@ -5,7 +5,9 @@ import structlog
 from typing import Literal, TypeAlias, get_args, Any
 from finlib.config import get_settings
 from finlib.decorators import async_retry
-from datetime import datetime
+from datetime import datetime, timedelta
+import pandas as pd
+from decimal import Decimal
 
 log = structlog.get_logger(__name__)
 
@@ -13,43 +15,45 @@ binance_interval: TypeAlias = Literal["1m", "3m", "5m", "15m", "30m", "1h", "2h"
 
 class BinanceDataRow(BaseModel):
     """Pydantic class to capture a Binance data row"""
-    open_time: int
-    open: str = Field(..., min_length=1)
-    high: str = Field(..., min_length=1)
-    low: str = Field(..., min_length=1)
-    close: str = Field(..., min_length=1)
-    volume: str = Field(..., min_length=1)
-    close_time: int
-    quote_asset_volume: str = Field(..., min_length=1)
+    open_time: datetime
+    open: Decimal = Field(..., gt=0)
+    high: Decimal = Field(..., gt=0)
+    low: Decimal = Field(..., gt=0)
+    close: Decimal = Field(..., gt=0)
+    volume: Decimal = Field(..., gt=0)
+    close_time: datetime
+    quote_asset_volume: Decimal = Field(..., gt=0)
     number_of_trades: int = Field(..., gt=0)
-    taker_buy_base_asset_volume: str = Field(..., min_length=1)
-    taker_buy_quote_asset_volume: str = Field(..., min_length=1)
+    taker_buy_base_asset_volume: Decimal = Field(..., gt=0)
+    taker_buy_quote_asset_volume: Decimal = Field(..., gt=0)
     ignore: str
 
-    @field_validator("open_time", "close_time")
-    @classmethod
-    def valid_unix_timestamp(cls, n: int) -> int:
-        if n<int(datetime(2015,1,1).timestamp()*1000):
-            raise ValueError
-        return n
-
-async def fetch_binance_one_symbol_one_row(session: aiohttp.ClientSession, symbol: str, interval: binance_interval) -> Any:
+async def _fetch_binance_one_symbol(session: aiohttp.ClientSession, symbol: str, interval: binance_interval, limit: int) -> list[list[Any]]:
     """Coroutine to fetch one data for one symbol from Binance."""
     settings = get_settings()
-    log.info("Async fetching Binance data", interval=interval, symbol=symbol)
+    log.info("Async fetching Binance data", interval=interval, symbol=symbol, limit=limit)
     async with asyncio.timeout(settings.fetch_timeout_seconds):
-        async with session.get(settings.binance.url, params={"symbol": symbol, "interval": interval, "limit": 1}) as resp:
+        async with session.get(settings.binance.url, params={"symbol": symbol, "interval": interval, "limit": limit}) as resp:
             resp.raise_for_status()
             return await resp.json()
 
 @async_retry(max_retry=3, delay=1.)
-async def validated_fetch_binance_one_symbol_one_row(session: aiohttp.ClientSession, symbol: str, interval: binance_interval, semaphore: asyncio.Semaphore) -> BinanceDataRow | None:
+async def _validated_fetch_binance_one_symbol(session: aiohttp.ClientSession, symbol: str, interval: binance_interval, limit: int, semaphore: asyncio.Semaphore) -> list[BinanceDataRow] | None:
     """Coroutine to fetch one data for one symbol from Binance. Validated with Pydantic."""
     settings = get_settings()
     async with semaphore:
         try:
-            data = await fetch_binance_one_symbol_one_row(session, symbol, interval)
-            return BinanceDataRow(**{k: v for k, v in zip(settings.binance.columns, data[0])})
+            data = await _fetch_binance_one_symbol(session, symbol, interval, limit)
+            result = []
+            invalid_rows_count=0
+            for row in data:
+                try:
+                    result.append(BinanceDataRow(**{k: v for k, v in zip(settings.binance.columns, row)}))
+                except ValidationError as e:
+                    invalid_rows_count+=0
+            if invalid_rows_count>0:
+                log.warning("Invalid Binance data", symbol=symbol, invalid_rows_count=invalid_rows_count)
+            return result
         except (ValidationError, aiohttp.client_exceptions.ClientResponseError) as e:
             log.warning("Bad response", url=settings.binance.url, symbol=symbol, exc=e)
             return None
@@ -57,7 +61,7 @@ async def validated_fetch_binance_one_symbol_one_row(session: aiohttp.ClientSess
             log.warning("Missing data", url=settings.binance.url, symbol=symbol, exc=e)
             return None
 
-async def fetch_binance_data(symbols: list[str], interval: binance_interval) -> dict[str, BinanceDataRow | None]:
+async def _fetch_binance_raw_data(symbols: list[str], interval: binance_interval, limit: int) -> dict[str, list[BinanceDataRow] | None]:
     """Coroutine to fetch multiple symbols from Binance"""
     if not isinstance(symbols, list):
         raise TypeError
@@ -66,8 +70,38 @@ async def fetch_binance_data(symbols: list[str], interval: binance_interval) -> 
     settings = get_settings()
     semaphore = asyncio.Semaphore(settings.binance.max_number_concurrent_calls)    
     async with aiohttp.ClientSession() as session:
-        result = await asyncio.gather(*[validated_fetch_binance_one_symbol_one_row(session, symbol, interval, semaphore) for symbol in symbols])
+        result = await asyncio.gather(*[_validated_fetch_binance_one_symbol(session, symbol, interval, limit, semaphore) for symbol in symbols])
     return {s:r for s,r in zip(symbols, result)}
 
-def stream_binance_data(symbols: list[str], interval: binance_interval) -> dict[str, BinanceDataRow | None]:
-    return asyncio.run(fetch_binance_data(symbols, interval))
+def _convert_binance_data_to_DataFrame(data: dict[str, list[BinanceDataRow] | None]) -> pd.DataFrame:
+    result = pd.DataFrame()
+    columns = BinanceDataRow.model_fields
+    for symbol, symbol_data in filter(lambda d: d[1] is not None, data.items()):
+        df = (
+            pd.DataFrame([row.model_dump() for row in symbol_data], columns=columns)
+            .assign(symbol=symbol)
+            [["symbol", *columns]]
+            .drop(columns=["ignore"])
+            .astype({"symbol": "category"})
+        )
+        result = pd.concat((result, df), axis=0)
+    return result.sort_values(["open_time", "symbol"])
+
+def fetch_binance(symbols: list[str], interval: binance_interval, start: datetime | str) -> pd.DataFrame:
+    match interval[-1]:
+        case "s":
+            deltat = timedelta(seconds=float(interval[:-1]))
+        case "m":
+            deltat = timedelta(minutes=float(interval[:-1]))
+        case "h":
+            deltat = timedelta(hours=float(interval[:-1]))
+        case "d":
+            deltat = timedelta(days=float(interval[:-1]))
+        case _:
+            raise NotImplementedError
+
+    start = datetime.strptime(start) if isinstance(start, str) else start
+    limit = (datetime.now() - start) / deltat
+
+    data = asyncio.run(_fetch_binance_raw_data(symbols, interval, limit))
+    return _convert_binance_data_to_DataFrame(data)
--- tests/test_async_fetch.py
+++ tests/test_async_fetch.py
@@ -1,38 +1,43 @@
-from finlib.async_fetch import stream_binance_data, fetch_binance_data
+from finlib.async_fetch import BinanceDataRow, fetch_binance, _fetch_binance_raw_data, _convert_binance_data_to_DataFrame
 import pytest
 from unittest.mock import AsyncMock, patch
 import aiohttp
 import asyncio
 from finlib.config import get_settings
+from datetime import datetime
+from decimal import Decimal
+
+_EX_DT = datetime(2026,7,1,0,0,0)
+_EX_DEC = Decimal(103.5)
+_EX_INT = 1214
+_EX_STR = "0"
+
+_EX_ROW = [_EX_DT, _EX_DEC, _EX_DEC, _EX_DEC, _EX_DEC, _EX_DEC, _EX_DT, _EX_DEC, _EX_INT, _EX_DEC, _EX_DEC, _EX_STR]
 
 def test_stream_binance_data_interval():
     with pytest.raises(ValueError):
-        _ = stream_binance_data(["BTCUSDT"], "13m")
+        _ = fetch_binance(["BTCUSDT"], "13m", _EX_DT)
 
 def test_stream_binance_data_symbol_type():
     with pytest.raises(TypeError):
-        _ = stream_binance_data("BTCUSDT", "1m")
+        _ = fetch_binance("BTCUSDT", "1m", _EX_DT)
 
 async def test_fetch_binance_data_happy_path():
-    with patch('finlib.async_fetch.fetch_binance_one_symbol_one_row', new_callable=AsyncMock) as mock:
-        mocked_output = [[1782707280000, '59969.64000000','59972.00000000','59941.03000000', '59941.03000000', '7.63798000', 
-                          1782707339999, '458032.05198250', 1714, '0.66735000', '40088.75994900', '0']]
-        mock.return_value = mocked_output
-        result = await fetch_binance_data(['SYM'], "1m")
-    assert result is not None and result['SYM'].open_time == 1782707280000
+    with patch('finlib.async_fetch._fetch_binance_one_symbol', new_callable=AsyncMock) as mock:
+        mock.return_value = [_EX_ROW]
+        result = await _fetch_binance_raw_data(['SYM'], "1m", 1)
+    assert (len(result["SYM"])==1) and (result["SYM"] is not None) and (result['SYM'][0].open_time == _EX_DT)
 
 async def test_fetch_binance_data_malformed_data():
-    with patch('finlib.async_fetch.fetch_binance_one_symbol_one_row', new_callable=AsyncMock) as mock:
-        mocked_output = [['59969.64000000','59972.00000000','59941.03000000', '59941.03000000', '7.63798000', 
-                          1782707339999, '458032.05198250', 1714, '0.66735000', '40088.75994900', '0']]
-        mock.return_value = mocked_output
-        result = await fetch_binance_data(['SYM'], "1m")
-    assert result['SYM'] is None
+    with patch('finlib.async_fetch._fetch_binance_one_symbol', new_callable=AsyncMock) as mock:
+        mock.return_value = [_EX_ROW, _EX_ROW[1:]]
+        result = await _fetch_binance_raw_data(['SYM'], "1m", 2)
+    assert (len(result["SYM"])==1) and (result['SYM'][0].open_time == _EX_DT)
 
 async def test_fetch_binance_data_client_response_error():
-    with patch('finlib.async_fetch.fetch_binance_one_symbol_one_row', new_callable=AsyncMock) as mock:
+    with patch('finlib.async_fetch._fetch_binance_one_symbol', new_callable=AsyncMock) as mock:
         mock.side_effect = aiohttp.client_exceptions.ClientResponseError("x", "y")
-        result = await fetch_binance_data(['SYM'], "1m")
+        result = await _fetch_binance_raw_data(['SYM'], "1m",1)
     assert result["SYM"] is None
 
 async def test_fetch_binance_semaphore():
@@ -42,21 +47,20 @@ async def test_fetch_binance_semaphore():
     peak_active = 0
     lock = asyncio.Lock()
 
-    async def slow_fetch(session, symbol, interval):
+    async def slow_fetch(session, symbol, interval, limit):
         nonlocal active_count, peak_active
         async with lock:
             active_count+=1
             peak_active = max(peak_active, active_count)
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.02)
         async with lock:
             active_count-=1
-        return [[1782707280000, '59969.64000000','59972.00000000','59941.03000000', '59941.03000000', '7.63798000', 
-                          1782707339999, '458032.05198250', 1714, '0.66735000', '40088.75994900', '0']]
+        return [_EX_ROW]
 
-    with patch('finlib.async_fetch.fetch_binance_one_symbol_one_row', new_callable=AsyncMock) as mock:
+    with patch('finlib.async_fetch._fetch_binance_one_symbol', new_callable=AsyncMock) as mock:
         mock.side_effect = slow_fetch
         ten_symbols = [f"SYM{n}" for n in range(10)]
-        _ = await fetch_binance_data(ten_symbols, "1m")
+        _ = await _fetch_binance_raw_data(ten_symbols, "1m", 1)
     assert peak_active==max_concurrent
 
         
@@ -65,17 +69,21 @@ async def test_fetch_binance_retry():
     count = 0
     lock = asyncio.Lock()
 
-    async def retry_fetch(session, symbol, interval):
+    async def retry_fetch(session, symbol, interval, limit):
         nonlocal count
         async with lock:
             count +=1
         if count<max_retry:
             raise aiohttp.ClientError
         else:
-            return [[1782707280000, '59969.64000000','59972.00000000','59941.03000000', '59941.03000000', '7.63798000', 
-                          1782707339999, '458032.05198250', 1714, '0.66735000', '40088.75994900', '0']]
+            return [_EX_ROW]
 
-    with patch('finlib.async_fetch.fetch_binance_one_symbol_one_row', new_callable=AsyncMock) as mock:
+    with patch('finlib.async_fetch._fetch_binance_one_symbol', new_callable=AsyncMock) as mock:
         mock.side_effect = retry_fetch
-        _ = await fetch_binance_data(["SYM"], "1m")
+        _ = await _fetch_binance_raw_data(["SYM"], "1m", 1)
     assert count==max_retry
+
+
+def test_convert_binance_data_to_DataFrame_symbol_data_missing():
+    brow = BinanceDataRow(**{k:v for k,v in zip(BinanceDataRow.model_fields, _EX_ROW)})
+    assert (_convert_binance_data_to_DataFrame({"SYM1": [brow], "SYM2": None}) == _convert_binance_data_to_DataFrame({"SYM1": [brow]})).all().all()
