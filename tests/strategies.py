659d56235cda7c7c2c96e5f5f259c807bcb64788
simone <284838112+bellinquente-a11y@users.noreply.github.com>
2026-07-19 19:07:13 +1000

    test: add hypothesis-based tests for portfolio functionality using new hypothesis strategies

---
+++ tests/strategies.py
@@ -0,0 +1,39 @@
+from hypothesis import strategies as st
+from finlib import Trade
+from datetime import datetime, timedelta
+
+def trade_strategy():
+    return st.builds(
+        Trade,
+        symbol=st.text(alphabet=st.characters(categories=["Lu"]), min_size=1, max_size=10),
+        quantity=st.floats(min_value=0, max_value=1e9, exclude_min=True),
+        price=st.floats(min_value=0, max_value=1e9, exclude_min=True),
+        side=st.sampled_from(['BUY', 'SELL']),
+    )
+
+@st.composite
+def ordered_trades_list(draw, min_trades=1, max_trades=20):
+    symbols = draw(st.lists(
+        st.text(alphabet=st.characters(categories=["Lu"]), min_size=1, max_size=10),
+        min_size=1, max_size=5, unique=True
+    ))
+
+    n_trades = draw(st.integers(min_value=min_trades, max_value=max_trades))
+
+    timestamp = draw(st.datetimes(min_value=datetime(2020,1,1), max_value=datetime(2030,1,1)))
+    res = []
+    for _ in range(n_trades):
+        offset = draw(st.integers(min_value=1, max_value=3600))
+        timestamp += timedelta(offset)
+        res.append(
+            Trade(
+                symbol=draw(st.sampled_from(symbols)),
+                quantity=draw(st.floats(min_value=0, max_value=1e9, exclude_min=True)),
+                price=draw(st.floats(min_value=0, max_value=1e9, exclude_min=True)),
+                side=draw(st.sampled_from(['BUY', 'SELL'])),
+                timestamp=timestamp
+            )
+        )
+
+    return res
+
--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -4,6 +4,10 @@ from decimal import Decimal
 import pandas as pd
 import pytest
 from datetime import datetime
+from hypothesis import given
+from tests.strategies import trade_strategy, ordered_trades_list
+from itertools import groupby
+import random
 
 def test_portfolio_empty_trade_list_validation_error():
     with pytest.raises(ValidationError):
@@ -19,9 +23,21 @@ def test_value_portfolio_calculation():
 def test_portfolio_len(sample_portfolio):
     assert len(sample_portfolio)==6
 
+@given(trades=ordered_trades_list())
+def test_portfolio_hypothesis_len(trades):
+    portfolio = Portfolio(name="MyPortfolio", trades=trades)
+    assert len(portfolio)==len(trades)
+
 @pytest.mark.parametrize("symbol,expected", [("BBB", True), ("CBA", False)], ids=["present", "missing"])
 def test_portfolio_contains(sample_portfolio, symbol, expected):
     assert (symbol in sample_portfolio) == expected
+
+@given(trades=ordered_trades_list())
+def test_portfolio_hypothesis_contains(trades):
+    portfolio = Portfolio(name="MyPortfolio", trades=trades)
+    symbols = set([t.symbol for t in trades])
+    for s in symbols:
+        assert s in portfolio
 
 def test_portfolio_name_missing(sample_trades):
     with pytest.raises(ValidationError):
@@ -37,7 +53,18 @@ def test_portfolio_get_item_out_of_range(sample_portfolio):
         sample_portfolio[99]
 
 def test_portfolio_notional_calculation(sample_portfolio):
-    assert abs(sample_portfolio.notional-Decimal(1_281.41)) <1e-12
+    assert sample_portfolio.notional == pytest.approx(Decimal(1_281.41))
+
+@given(trades=ordered_trades_list())
+def test_portfolio_hypothesis_notional_calculation(trades):
+    portfolio = Portfolio(name="MyPortfolio", trades=trades)
+    assert portfolio.notional == pytest.approx(sum([t.notional for t in trades]))
+
+@given(trades=ordered_trades_list())
+def test_portfolio_hypothesis_notional_calculation_independent_of_permutations(trades):
+    portf1 = Portfolio(name="MyPortfolio", trades=trades)
+    portf2 = Portfolio(name="MyPortfolio", trades=random.sample(trades, len(trades)))
+    assert portf1.notional == pytest.approx(portf2.notional)
 
 def test_portfolio_historic_position(sample_historic_portfolio):
     pos = sample_historic_portfolio.historic_position()
@@ -50,6 +77,23 @@ def test_portfolio_historic_position(sample_historic_portfolio):
     ], 
     columns=["AAA", "BHP"], index = sorted([t.timestamp for t in sample_historic_portfolio]))
     assert (pos==exp_pos).all().all()
+
+@given(trades=ordered_trades_list())
+def test_portfolio_historic_position_hypothesis(trades):
+    portfolio = Portfolio(name="MyPortfolio", trades=trades)
+    trades = sorted(trades, key=lambda t: t.symbol)
+    positions = {}
+    for key, group in groupby(trades, key=lambda t: t.symbol):
+        positions[key] = sum([t.lot_size() for t in list(group)])
+    exp_pos_df = pd.Series(positions)
+    pos_df =  portfolio.historic_position().iloc[-1]
+    assert (pos_df == exp_pos_df).all()
+
+@given(trades=ordered_trades_list())
+def test_portfolio_final_position_hypothesis_independent_permutations_trades(trades):
+    portf1 = Portfolio(name="MyPortfolio", trades=trades)
+    portf2 = Portfolio(name="MyPortfolio", trades=random.sample(trades, len(trades)))
+    assert (portf1.historic_position().iloc[-1] == portf2.historic_position().iloc[-1]).all()
 
 def test_portfolio_historic_cost_basis(sample_historic_portfolio):
     cost = sample_historic_portfolio.historic_cost_basis()
