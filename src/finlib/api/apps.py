8927338f4c1ecc4dba3169ff1ce6415e3e67be19
simone <284838112+bellinquente-a11y@users.noreply.github.com>
2026-08-05 15:19:56 +1000

    feat: implement lifespan management for FastAPI with market data and trade repository initialization

--- src/finlib/api/apps.py
+++ src/finlib/api/apps.py
@@ -1,7 +1,45 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from pathlib import Path
+
 from fastapi import FastAPI
 
 from finlib.api.routes import market_data, portfolio
+from finlib.config import get_settings
+from finlib.ohlcv_repo import FileOHLCVRepository, SQLiteOHLCVRepository
+from finlib.trade_repo import FileTradeRepository, SQLiteTradeRepository
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    db_dir = get_settings().data_dir
+    db_dir.mkdir(parents=True, exist_ok=True)
+
+    repo_root = Path(__file__).resolve().parents[3]
+    file_trade_repo_path = repo_root / "examples" / "trades.jsonl"
+    file_mkt_repo_path = repo_root / "examples" / "mkt_data_1h.csv"
+    db_trade_repo_path = db_dir / "trades.db"
+    db_mkt_repo_path = db_dir / "mkt_data_1h.db"
+
+    # Make sure the files exist
+    if not file_trade_repo_path.exists():
+        raise ValueError(f"Missing file {file_trade_repo_path}")
+    if not file_mkt_repo_path.exists():
+        raise ValueError(f"Missing file {file_mkt_repo_path}")
+
+    # Clone repos
+    trade_repo_file = FileTradeRepository(file_trade_repo_path)
+    trade_repo_db = SQLiteTradeRepository(db_trade_repo_path)
+    for trade in trade_repo_file.get_all():
+        trade_repo_db.add(trade)
+
+    mkt_repo_file = FileOHLCVRepository(file_mkt_repo_path)
+    mkt_repo_db = SQLiteOHLCVRepository(db_mkt_repo_path)
+    for symbol in mkt_repo_file.symbols():
+        mkt_repo_db.add_intervals_batch(mkt_repo_file.get_data(symbol))
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(market_data.router)
 app.include_router(portfolio.router)
